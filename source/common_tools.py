61605e54e39f7a135f59d6720098560522014894
Danika MacDonell <danikam@mit.edu>
2024-08-06 15:13:19 -0400

    Code cleanup with ruff, and initial commit of code to produce output csv files

--- source/common_tools.py
+++ source/common_tools.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 import os
+
+
 def get_top_dir():
-    '''
+    """
     Gets the path to the top level of the git repo (one level up from the source directory)
-        
+
     Parameters
     ----------
     None
@@ -11,9 +13,9 @@ def get_top_dir():
     Returns
     -------
     top_dir (string): Path to the top level of the git repo
-        
+
     NOTE: None
-    '''
+    """
     source_path = Path(__file__).resolve()
     source_dir = source_path.parent
     top_dir = os.path.dirname(source_dir)
--- source/make_cost_emissions_files.py
+++ source/make_cost_emissions_files.py
@@ -7,9 +7,10 @@ import pandas as pd
 import os
 from common_tools import get_top_dir
 
+
 # Function to create .inc file content
 def create_inc_file_content(row):
-    pathway_type = row['Pathway Name'].split('-')[0]
+    pathway_type = row["Pathway Name"].split("-")[0]
     content = f"""# Definition of cost and emissions for {row['Fuel']} from {row['Region']} using {row['Pathway Description']}
 
 Port "port" {{
@@ -24,23 +25,24 @@ set_bunker_price_overwrite("{row['Fuel']}_{pathway_type}", {row['Cost [$/tonne]'
 """
     return content
 
+
 def main():
     top_dir = get_top_dir()
-    input_dir = f'{top_dir}/input_fuel_pathway_data/'
-    output_dir = f'{top_dir}/includes_global/all_costs_emissions/'
+    input_dir = f"{top_dir}/input_fuel_pathway_data/"
+    output_dir = f"{top_dir}/includes_global/all_costs_emissions/"
 
     # Ensure output directory exists
     os.makedirs(output_dir, exist_ok=True)
 
     # Delete all existing .inc files in the output directory
     for file_name in os.listdir(output_dir):
-        if file_name.endswith('.inc'):
+        if file_name.endswith(".inc"):
             os.remove(os.path.join(output_dir, file_name))
             print(f"Deleted file: {file_name}")
 
     # Loop through all CSV files in the input directory
     for file_name in os.listdir(input_dir):
-        if file_name.endswith('.csv'):
+        if file_name.endswith(".csv"):
             csv_file = os.path.join(input_dir, file_name)
             df = pd.read_csv(csv_file)
 
@@ -50,14 +52,11 @@ def main():
                 file_content = create_inc_file_content(row)
 
                 # Write content to .inc file
-                with open(inc_file_name, 'w') as file:
+                with open(inc_file_name, "w") as file:
                     file.write(file_content)
 
                 print(f"File created: {inc_file_name}")
 
+
 if __name__ == "__main__":
     main()
-
-
-
-
