429d3dc523b139b2b3aa64411883f3333129a663
Danika MacDonell <danikam@mit.edu>
2024-08-22 17:50:10 -0400

    Updated run_all_pathways.sh to reflect new color-excluding pathway naming convention

--- run_all_pathways.sh
+++ run_all_pathways.sh
@@ -24,21 +24,20 @@ rm ${SCRIPT_DIR}/all_outputs_full_fleet/*.xlsx
 
 echo 'Processing lsfo pathway'
 # Copy the given black pathway into the top level of includes_global and run
-navigate --suppress-plots ${SCRIPT_DIR}/single_pathway_full_fleet/lsfo_pathway/lsfo_pathway.nav
-cp ${SCRIPT_DIR}/single_pathway_full_fleet/lsfo_pathway/plots/lsfo_pathway_excel_report.xlsx ${SCRIPT_DIR}/all_outputs_full_fleet/report_lsfo-1.xlsx
+navigate --suppress-plots ${SCRIPT_DIR}/single_pathway_full_fleet/lsfo/lsfo.nav
+cp ${SCRIPT_DIR}/single_pathway_full_fleet/lsfo/plots/lsfo_excel_report.xlsx ${SCRIPT_DIR}/all_outputs_full_fleet/report-lsfo-1.xlsx
 
 process_pathway() {
     local inc_file=$1
-    local fuel_type=$2
-    local pathway_type=$3
-    local pathway_name=$4
+    local fuel=$2
+    local pathway_name=$3
 
     echo "Processing ${pathway_name} pathway"
 
-    cp "$inc_file" "${DEST_DIR}/cost_emissions_${fuel_type}_${pathway_type}.inc"
-    echo navigate --suppress-plots "${SCRIPT_DIR}/single_pathway_full_fleet/${fuel_type}_${pathway_type}_pathway/${fuel_type}_${pathway_type}_pathway.nav"
-    navigate --suppress-plots "${SCRIPT_DIR}/single_pathway_full_fleet/${fuel_type}_${pathway_type}_pathway/${fuel_type}_${pathway_type}_pathway.nav"
-    cp "${SCRIPT_DIR}/single_pathway_full_fleet/${fuel_type}_${pathway_type}_pathway/plots/${fuel_type}_${pathway_type}_pathway_excel_report.xlsx" "${SCRIPT_DIR}/all_outputs_full_fleet/report_${pathway_name}.xlsx"
+    cp "$inc_file" "${DEST_DIR}/cost_emissions_${fuel}.inc"
+    echo navigate --suppress-plots "${SCRIPT_DIR}/single_pathway_full_fleet/${fuel}/${fuel}.nav"
+    navigate --suppress-plots "${SCRIPT_DIR}/single_pathway_full_fleet/${fuel}/${fuel}.nav"
+    cp "${SCRIPT_DIR}/single_pathway_full_fleet/${fuel}/plots/${fuel}_excel_report.xlsx" "${SCRIPT_DIR}/all_outputs_full_fleet/report-${pathway_name}.xlsx"
 }
 
 export -f process_pathway
@@ -49,13 +48,10 @@ for inc_file in "${SOURCE_DIR}"/*.inc; do
 
     filename=$(basename -- "$inc_file")
     fuel=$(echo $filename | cut -d'-' -f1)
-    pathway_type=$(echo $filename | cut -d'-' -f2)
     pathway_name=$(basename -- "$filename" .inc)
     
-    echo $filename $fuel $pathway_type $pathway_name
-
-    if [[ $pathway_type == "blue" || $pathway_type == "electro" || $pathway_type == "grey" ]]; then
-        process_pathway "$inc_file" "$fuel" "$pathway_type" "$pathway_name"
-    fi
+    echo $filename $fuel $pathway_name
+    process_pathway "$inc_file" "$fuel" "$pathway_name"
+    
 done
 
