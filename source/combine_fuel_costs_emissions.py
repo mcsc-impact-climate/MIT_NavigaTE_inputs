e04c8a52af3023e05c7e75566d4a6e9390b867bc
Danika MacDonell <danikam@mit.edu>
2025-12-27 14:44:34 -0500

    Updates to combine_fuel_costs_emissions.py to clean up output file

--- source/combine_fuel_costs_emissions.py
+++ source/combine_fuel_costs_emissions.py
@@ -31,16 +31,16 @@ def add_transportation_storage_costs(costs_emissions_df, fuel, port):
     transport_df = pd.read_csv(transport_file)
     
     merged_df = pd.merge(costs_emissions_df, transport_df, on=["Region"], how="left")
-
-    print(merged_df.columns)        
     
     # Make a final LCOF and Emissions column that sums up all relevant costs and emissions
     merged_df["Final LCOF [$/tonne]"] = merged_df["LCOF [$/tonne]"] + merged_df["Land Transport Cost [$/tonne]"] + merged_df["Fuel Storage Cost [$/tonne]"] + merged_df["Tanker Transport Cost [$/tonne]"]
     merged_df["Final Emissions [kg CO2e / kg fuel]"] = merged_df["Emissions [kg CO2e / kg fuel]"] + merged_df["Land Transport Emissions [kg CO2e / kg fuel]"] + merged_df["Fuel Storage Emissions [kg CO2e / kg fuel]"] + merged_df["Tanker Transport Emissions [kg CO2e / kg fuel]"]
 
     # Rename LCOF [$/tonne] to Fuel Production LCOF [$/tonne] and Emissions [kg CO2e / kg fuel] to Fuel Production Emissions [kg CO2e / kg fuel] for clarity
-    merged_df.rename(columns={"LCOF [$/tonne]": "Fuel Production LCOF [$/tonne]", "Emissions [kg CO2e / kg fuel]": "Fuel Production Emissions [kg CO2e / kg fuel]"}, inplace=True)
-    
+    merged_df.rename(columns={"LCOF [$/tonne]": "Fuel Production LCOF [$/tonne]", "Emissions [kg CO2e / kg fuel]": "Fuel Production Emissions [kg CO2e / kg fuel]", "Fuel_x": "Fuel"}, inplace=True)
+    merged_df.drop(columns=["Fuel_y", "Comment"], inplace=True)
+    print(merged_df.columns)
+
     return merged_df
 
 def main():
@@ -51,14 +51,16 @@ def main():
         if result:
             fuel = result["fuel"]
             if fuel in fuels:
-                costs_emissions_df = pd.read_csv(os.path.join(top_dir, "input_fuel_pathway_data", "production", file_name))
                 for port in ports:
+                    costs_emissions_df = pd.read_csv(os.path.join(top_dir, "input_fuel_pathway_data", "production", file_name))
                     costs_emissions_df = add_transportation_storage_costs(costs_emissions_df, fuel, port)
 
                     # Make sure the output directory exists
                     output_dir = os.path.join(top_dir, "final_fuel", port)
                     os.makedirs(output_dir, exist_ok=True)
-                    costs_emissions_df.to_csv(os.path.join(output_dir, f"{fuel}_{port}_costs_emissions.csv"), index=False)
+                    filepath_save = os.path.join(output_dir, f"{fuel}_{port}_costs_emissions.csv")
+                    costs_emissions_df.to_csv(filepath_save, index=False)
+                    print(f"Combined cost-emissions table saved to {filepath_save}")
 
 if __name__ == "__main__":
     main()
